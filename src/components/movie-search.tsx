4119de92789f0d2e0c2cba7c8a4fe8ffeaa611c6
Orkuma Ivo <orkumaivo@Orkumas-MBP-2.broadband>
2025-04-18 23:27:35 +0100

    Refactor MovieHeader, MovieItem, and MovieSearch components to use ClickableText for navigation and improve layout. Added back navigation in MovieHeader and updated search handling in MovieSearch for better user experience.

--- src/components/movie-search.tsx
+++ src/components/movie-search.tsx
@@ -4,13 +4,28 @@ import { Description, Field, Input, Label } from "@headlessui/react";
 import clsx from "clsx";
 
 import { GET_MOVIE } from "@/utils/grapqhl/movies";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Movie } from "@/types/movie";
 import { MovieItem } from "./movie-item";
 import { DataComponent } from "./elements/data-component";
+import { useRouter } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
+
 export function MovieSearch() {
-  const [search, setSearch] = useState("Jaws");
   const [tempSearchValue, setTempSearchValue] = useState("");
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [search, setSearch] = useState(searchParams.get("search") || "");
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
 
   return (
     <div className="w-64">
@@ -31,6 +46,9 @@ export function MovieSearch() {
               if (tempSearchValue.trim().length === 0) return;
               if (e.key === "Enter") {
                 setSearch(tempSearchValue);
+                router.push(
+                  pathname + "?" + createQueryString("search", tempSearchValue)
+                );
               }
             }}
           />
