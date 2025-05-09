2fed960030f22b3cd1ea8c1e792d5a712d4fa1d2
Orkuma Ivo <orkumaivo@Orkumas-MBP-2.broadband>
2025-05-09 09:40:30 +0100

    Remove weather icon from the next day's weather component

--- src/components/weather/weather-next-day.tsx
+++ src/components/weather/weather-next-day.tsx
@@ -2,7 +2,6 @@ export const WeatherNextDays = () => {
   return (
     <div className="text-center mb-0 flex items-center justify-center flex-col">
       <span className="block my-1">Sun</span>
-      <img src="https://i.imgur.com/ffgW9JQ.png" className="block w-8 h-8" />
       <span className="block my-1">38.3&deg;</span>
     </div>
   );
